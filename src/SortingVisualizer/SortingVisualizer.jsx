3f6e67726dc45bc6a2875bde75d48e204d45be0a
Steven <stevnn.yee@gmail.com>
2024-10-03 23:02:22 -0400

    pretty chart

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -16,7 +16,7 @@ export default class SortingVisualizer extends React.Component {
 
     resetArray() {
         const array = [];
-        for(let i = 0; i < 100; i++) {
+        for(let i = 0; i < 310; i++) {
             array.push(randomIntFromInterval(10, 1000));
         }
         this.setState({array});
@@ -26,13 +26,15 @@ export default class SortingVisualizer extends React.Component {
         const {array} = this.state;
 
         return (
-            <>
+            <div className="array-container">
                 {array.map((value, idx) => (
-                    <div className = "array-bar" key={idx}>
-                        {value}
+                    <div  
+                        className = "array-bar" 
+                        key={idx}
+                        style={{height: `${value}px`}}>
                     </div>
                 ))}
-            </>
+            </div>
             );
         }
     }
